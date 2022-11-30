e561d417088acb08672fce693477fbbcb465a29a
MarlyPabon <franklinortiz@wposs.com>
2022-11-30 17:01:05 -0500

    actualizaTRES

--- programa20.c
+++ programa20.c
@@ -3,18 +3,15 @@
 
 int main() {
 
-     int dd, mm, aa;
+    int dd, mm, aa;
     printf("Ingrese el dia: ");
     scanf("%i", &dd);
     printf("Ingrese el mes: ");
     scanf("%i", &mm);
     printf("Ingrese el año: ");
     scanf("%i", &aa);
-    if(mm==1 || mm==2 || mm==3){
-        printf("La fecha ingresada corresponde al primer trimestre ");
-        printf("%i - ", dd);
-        printf("%i - ", mm);
-        printf("%i ", aa);
+    if(dd==25 && mm==12){
+        printf("La fecha ingresada corresponde a NAVIDAD. ");
     }
 
     getch();
