554e7a873f0776ba030fb8b128f473ebd2cbabd5
MarlyPabon <franklinortiz@wposs.com>
2022-11-30 16:51:31 -0500

    actualiza

---
+++ programa19.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+
+int main() {
+
+     int num1, num2, num3;
+    printf("Ingrese el primer valor: ");
+    scanf("%i", &num1);
+
+    getch();
+    return 0;
+}
